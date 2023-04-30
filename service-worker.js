18e97853655a28a889e0313daec57327d1883a87
Aadhiya <aadhiyamaria@gmail.com>
2023-04-30 04:30:48 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'audio-cache';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(['/audio.mp3']);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  const request = event.request;
+
+  if (request.method === 'GET' && request.url.endsWith('.mp3')) { // replace '.mp3' with the file type of your audio files
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match(request).then(function(response) {
+          if (response) {
+            return response;
+          }
+
+          return fetch(request).then(function(response) {
+            cache.put(request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  }
+});
